0a3652b8ccca63ac15b2a00a39cde5d8b771258c
clawdeck9 <clawdeck9@gmail.com>
2019-12-26 18:15:05 +0100

    now with a reactive form and a select input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { MyformComponent } from './myform/myform.component';
-import { MymodelComponent } from './mymodel/mymodel.component';
 import { ReactformComponent } from './reactform/reactform.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyformComponent,
-    MymodelComponent,
     ReactformComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/reactform/reactform.component.ts
+++ src/app/reactform/reactform.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Log } from '../log';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-reactform',
@@ -9,12 +10,32 @@ import { Log } from '../log';
 export class ReactformComponent implements OnInit {
   
   log = new Log("no tag", "no title");
-  tags = ["music", "java", "dp", "eds"];
-
-  constructor() { }
+  tags = [];
+  logFormGroup: FormGroup;
+  
+  constructor() {
+    //this.logFormGroup.controls.tags.patchValue(this.tags[0]);
+  }
 
   ngOnInit() {
-    // retrieve the tags with the restservice
+    this.logFormGroup  = new FormGroup({
+      'title': new FormControl(null),
+      'tag': new FormControl(null)
+    });
+    this.tags = this.getTags();
+  }
+
+  submit() {
+    console.log("submit is called:", this.logFormGroup.value);
+    
+  }
+
+  getTags(){
+    return ['music', 'eds', 'java', 'dp'];
   }
 
 }
+
+
+
+
