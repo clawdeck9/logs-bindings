4181e8cb1cef05d41167c7654a625599db8734b6
clawdeck9 <clawdeck9@gmail.com>
2019-12-30 17:54:15 +0100

    added a tag selector that filters the tag list with reactive form controls and observable.

--- src/app/reactform/reactform.component.ts
+++ src/app/reactform/reactform.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Log } from '../log';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
+import { stringify } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-reactform',
@@ -11,7 +12,9 @@ export class ReactformComponent implements OnInit {
   
   log = new Log("no tag", "no title");
   tags = [];
+  filteredTags = [];
   logFormGroup: FormGroup;
+  tagbeg: string;
   
   constructor() {
     //this.logFormGroup.controls.tags.patchValue(this.tags[0]);
@@ -19,19 +22,34 @@ export class ReactformComponent implements OnInit {
 
   ngOnInit() {
     this.logFormGroup  = new FormGroup({
-      'title': new FormControl(null),
+      'title': new FormControl('notitle'),
+      'tagbeg': new FormControl(null),
       'tag': new FormControl(null)
     });
-    this.tags = this.getTags();
+    this.tags = this.getNewTags();
+    this.filteredTags = this.getNewTags();
+    this.logFormGroup.valueChanges.subscribe( value => {
+      // filter the tag list
+      this.filteredTags = this.tags.filter(tag => tag.startsWith(value.tagbeg));
+      console.log("value of tagbeg : ", value.tagbeg);
+      console.log("filteredTags : ", this.filteredTags);
+      
+    });
   }
-
+// mettre ici une fonction qui prend en compte les changements de valeur
+// dans le control tagbeg et filtre la liste de tags en focntion
+  onChangeBeg(value: string){
+    this.tags.push(this.tagbeg);
+    console.log(value, this.tagbeg, this.tags);
+  }
   submit() {
     console.log("submit is called:", this.logFormGroup.value);
+    this.log.title = this.logFormGroup.value.title;
     
   }
 
-  getTags(){
-    return ['music', 'eds', 'java', 'dp'];
+  getNewTags(){
+    return ['music', 'eds', 'java', 'dp', 'js', 'jojo', 'jog'];
   }
 
 }
